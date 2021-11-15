a5b09aafb653fc2d27baaa4a3e48ec9104d88405
hien nguyen <thuhiennguyen198@gmail.com>
2021-11-15 06:45:17 -0500

    modified user validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,21 +6,23 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
+const passport = require("passport")
+
 
 router.get("/test", (req, res) => {
   res.json({ msg: "This is the users route" });
 });
-// router.get(
-//   "/current",
-//   passport.authenticate("jwt", { session: false }),
-//   (req, res) => {
-//     res.json({
-//       id: req.user.id,
-//       handle: req.user.handle,
-//       email: req.user.email,
-//     });
-//   }
-// );
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      handle: req.user.handle,
+      email: req.user.email,
+    });
+  }
+);
 
 router.post("/register", (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
@@ -30,8 +32,7 @@ router.post("/register", (req, res) => {
   // Check to make sure nobody has already registered with a duplicate email
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
-      errors.email = "Email already exists";
-      return res.status(400).json(errors);
+      return res.status(400).json({email: "exist"});
     } else {
       // Otherwise create a new user
       const newUser = new User({
@@ -64,27 +65,27 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then((user) => {
     if (!user) {
       // Throw a 400 error if the email address already exists
-      errors.email = "User not found";
+      return res.status(400).json({email: "User not found"});
     }
     bcrypt.compare(password, user.password).then((isMatch) => {
       if (isMatch) {
         res.json({ msg: "Success" });
-        // const payload = {
-        //   id: user.id,
-        //   handle: user.handle,
-        //   email: user.email,
-        // };
-        // jwt.sign(
-        //   payload,
-        //   keys.secretOrKey,
-        //   { expiresIn: 3600 },
-        //   (err, token) => {
-        //     res.json({
-        //       sucess: true,
-        //       token: "Bearer" + token,
-        //     });
-        //   }
-        // );
+        const payload = {
+          id: user.id,
+          handle: user.handle,
+          email: user.email,
+        };
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              sucess: true,
+              token: "Bearer" + token,
+            });
+          }
+        );
       } else {
         errors.password = "Incorrect password";
         return res.status(400).json(errors);
